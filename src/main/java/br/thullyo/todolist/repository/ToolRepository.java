9e312d356a8eaaa92ce80307f779a02e052437fd
Thullyoo <thullyofilho@gmail.com>
2024-07-16 14:15:48 -0300

    feat: adicionado metodo de busca por tag

--- src/main/java/br/thullyo/todolist/repository/ToolRepository.java
+++ src/main/java/br/thullyo/todolist/repository/ToolRepository.java
@@ -2,8 +2,15 @@ package br.thullyo.todolist.repository;
 
 import br.thullyo.todolist.model.Tool;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface ToolRepository extends JpaRepository<Tool, Integer> {
+
+    @Query(value = "SELECT * FROM tb_tools e WHERE :tag IN (SELECT unnest(e.tags))", nativeQuery = true)
+    List<Tool> findAllByTag(@Param("tag")String tag);
 }
